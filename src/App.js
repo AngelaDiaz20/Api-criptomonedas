07616ea334b2a975f4379da5f29277e72144845e
Angela Diaz <angeldi846@gmail.com>
2022-12-13 11:46:08 -0500

    Componentes de tabla y conexión a API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.css"; 
 import Footer from './Footer'
 import Header from './Header'
+import TableCoins from "./TableCoins";
 
 export default function App() {
+
+  const [coins, setCoins] = useState()
+
+  const getData = async () =>{
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d%2C%2014d%2C%2030d%2C%201y')
+    //console.log(response)
+    const json = await response.json()
+    //console.log(json)
+    setCoins(json)
+  }
+
+  useEffect (()=>{
+    getData()
+  },[])
+  
   return (
+    !coins ? "Cargando..." :(
     <div className='App'>
         <Header/>
+        <TableCoins coins={coins}/>
         <Footer/>
     </div>
+    )
   )
 }
 
--- src/CoinRow.jsx
+++ src/CoinRow.jsx
@@ -1,8 +1,19 @@
 import "./coinRow.css"
 
-export default function CoinRow() {
+export default function CoinRow({coin, index}) {
   return (
     <tr>
+      <td>{index}</td>
+      <td>
+        <div className="coin_image_container">
+          <img src={coin.image} alt={coin.id}/>
+        </div>
+      </td>
+      <td>{coin.current_price}US$</td>
+      <td>{coin.market_cap_change_percentage_24h}%</td>
+      <td>{coin.total_volume}US$</td>
+      <td>{coin.market_cap}US$</td>
+      <td>Gráfica</td>
     </tr>
   );
 }
